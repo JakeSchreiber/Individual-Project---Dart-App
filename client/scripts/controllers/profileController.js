954aaf9938d712031f3eef48af6694c4e9dbff87
JakeSchreiber <jake@jakeschreiber.com>
2015-12-01 08:29:00 -0600

    nested Promise

--- client/scripts/controllers/profileController.js
+++ client/scripts/controllers/profileController.js
@@ -1,25 +1,53 @@
 myApp.controller('ProfileController', ['$scope', '$http', function($scope, $http){
     console.log("Profile Controller");
 
+//    WORKS LOCALLY BUT NOT ONCE POSTED ON HEROKU
+
+//    $scope.getProfileInfo = function(){
+//        $http.get('/getprofile').then(function(response){
+//            $scope.profileInfo = response.data;
+//            console.log(response.data);
+//                checkUser();
+//        });
+//    };
+//
+//    var checkUser = function() {
+//        if ($scope.profileInfo.length === 0) {
+//            $http.post('/createuser').then(function (response) {
+//                $scope.profileInfo = response.data;
+//                console.log(response.data);
+//                console.log("Empty Array test fire");
+//                $scope.getProfileInfo();
+//
+//            });
+//        }
+//    };
+//
+//    $scope.getProfileInfo();
+//
+//
+//}]);
+
+
+
     $scope.getProfileInfo = function(){
         $http.get('/getprofile').then(function(response){
             $scope.profileInfo = response.data;
-            console.log(response.data);
-                checkUser();
+
+            if ($scope.profileInfo.length === 0) {
+                $http.post('/createuser').then(function (response) {
+                    $scope.profileInfo = response.data;
+                    console.log(response.data);
+                    console.log("Empty Array test fire");
+                    //$scope.getProfileInfo();
+
         });
     };
 
-    var checkUser = function() {
-        if ($scope.profileInfo.length === 0) {
-            $http.post('/createuser').then(function (response) {
-                $scope.profileInfo = response.data;
-                console.log(response.data);
-                console.log("Empty Array test fire");
-                $scope.getProfileInfo();
+
 
             });
         }
-    };
 
     $scope.getProfileInfo();
 
