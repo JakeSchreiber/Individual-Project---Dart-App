78168f60d051dc164ed7cf03f970117aecced5d3
JakeSchreiber <jake@jakeschreiber.com>
2015-12-01 10:52:03 -0600

    Heroku Check

--- client/scripts/controllers/profileController.js
+++ client/scripts/controllers/profileController.js
@@ -3,58 +3,58 @@ myApp.controller('ProfileController', ['$scope', '$http', function($scope, $http
 
 //    WORKS LOCALLY BUT NOT ONCE POSTED ON HEROKU
 
-//    $scope.getProfileInfo = function(){
-//        $http.get('/getprofile').then(function(response){
-//            $scope.profileInfo = response.data;
-//            console.log(response.data);
-//                checkUser();
-//        });
-//    };
-//
-//    var checkUser = function() {
-//        if ($scope.profileInfo.length === 0) {
-//            $http.post('/createuser').then(function (response) {
-//                $scope.profileInfo = response.data;
-//                console.log(response.data);
-//                console.log("Empty Array test fire");
-//                $scope.getProfileInfo();
-//
-//            });
-//        }
-//    };
-//
-//    $scope.getProfileInfo();
-//
-//
-//}]);
-
-
-
     $scope.getProfileInfo = function(){
         $http.get('/getprofile').then(function(response){
-            //$scope.profileInfo = response.data;
-            return checkUser(response);
-
-
-
+            $scope.profileInfo = response.data;
+            console.log(response.data);
+                checkUser();
         });
     };
-    var checkUser = function(response){
-        if (response.data.length === 0) {
+
+    var checkUser = function() {
+        if ($scope.profileInfo.length === 0) {
             $http.post('/createuser').then(function (response) {
                 $scope.profileInfo = response.data;
                 console.log(response.data);
                 console.log("Empty Array test fire");
-                //$scope.getProfileInfo();
+                $scope.getProfileInfo();
+
             });
-        } else {
-                $scope.profileInfo = response.data;
-
-            }
         }
-
+    };
 
     $scope.getProfileInfo();
 
 
 }]);
+
+//WORKS IN HEROKU
+
+//    $scope.getProfileInfo = function(){
+//        $http.get('/getprofile').then(function(response){
+//            //$scope.profileInfo = response.data;
+//            return checkUser(response);
+//
+//
+//
+//        });
+//    };
+//    var checkUser = function(response){
+//        if (response.data.length === 0) {
+//            $http.post('/createuser').then(function (response) {
+//                $scope.profileInfo = response.data;
+//                console.log(response.data);
+//                console.log("Empty Array test fire");
+//                //$scope.getProfileInfo();
+//            });
+//        } else {
+//                $scope.profileInfo = response.data;
+//
+//            }
+//        }
+//
+//
+//    $scope.getProfileInfo();
+//
+//
+//}]);
