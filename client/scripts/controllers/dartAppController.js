573ea010ccecd23a1bebb822a8b95c65381cf847
JakeSchreiber <jake@jakeschreiber.com>
2015-12-02 16:39:40 -0600

    Heroku test

--- client/scripts/controllers/dartAppController.js
+++ client/scripts/controllers/dartAppController.js
@@ -11,38 +11,38 @@ myApp.controller('DartController', ['$scope','$http', '$interval', 'PlayerServic
 
         //GET PROFILE USER from login and enter them to playerArray[0]/////
 
-        $scope.getLoggedInPlayer = function () {
-            var promise = $http.get('/getloggedinplayer').then(
-                function (response) {
-                    $scope.loggedInPlayer = response.data;
-                    $scope.playerArray = $scope.loggedInPlayer;
-                    console.log("Logged in player line 19:", $scope.loggedInPlayer);
-                    console.log("PlayerArray line 20:", $scope.playerArray);
-            });
-
-            return promise;
-        };
-
-        $scope.getLoggedInPlayer();
-
-
-        $scope.playerArray[0].roundArray = [];
-        $scope.playerArray[0].dartArray = [];
-
-        //WORKING VERSION LOCALLY NOT TO HEROKU
         //$scope.getLoggedInPlayer = function () {
-        //    $http.get('/getloggedinplayer').then(function (response) {
-        //        $scope.loggedInPlayer = response.data;
-        //        $scope.playerArray = $scope.loggedInPlayer;
-        //        $scope.playerArray[0].roundArray = [];
-        //        $scope.playerArray[0].dartArray = [];
-        //        console.log("Logged in player line 19:", $scope.loggedInPlayer);
-        //        console.log("PlayerArray line 20:", $scope.playerArray);
-        //
+        //    var promise = $http.get('/getloggedinplayer').then(
+        //        function (response) {
+        //            $scope.loggedInPlayer = response.data;
+        //            $scope.playerArray = $scope.loggedInPlayer;
+        //            console.log("Logged in player line 19:", $scope.loggedInPlayer);
+        //            console.log("PlayerArray line 20:", $scope.playerArray);
         //    });
+        //
+        //    return promise;
         //};
         //
         //$scope.getLoggedInPlayer();
+        //
+        //
+        //$scope.playerArray[0].roundArray = [];
+        //$scope.playerArray[0].dartArray = [];
+
+        //WORKING VERSION LOCALLY NOT TO HEROKU
+        $scope.getLoggedInPlayer = function () {
+            $http.get('/getloggedinplayer').then(function (response) {
+                $scope.loggedInPlayer = response.data;
+                $scope.playerArray = $scope.loggedInPlayer;
+                $scope.playerArray[0].roundArray = [];
+                $scope.playerArray[0].dartArray = [];
+                console.log("Logged in player line 19:", $scope.loggedInPlayer);
+                console.log("PlayerArray line 20:", $scope.playerArray);
+
+            });
+        };
+
+        $scope.getLoggedInPlayer();
 
 
 
